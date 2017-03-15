fbb63226a6f61ed7b9b502942afbaa708440874c
wakabashi <wkba61@gmail.com>
2017-03-15 16:38:05 +0900

    end

--- animations/ViewController.swift
+++ animations/ViewController.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2017年 wakabayashi. All rights reserved.
 //
 
+import Lottie
 import UIKit
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -15,11 +15,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func showAnimation(_ sender: Any) {
+        let animationView = LOTAnimationView.init(name: "PinJump")
+        animationView?.frame = CGRect(x: 0, y: 100, width: self.view.frame.size.width, height: 250)
+        animationView?.contentMode = .scaleAspectFit
+        animationView?.loopAnimation = true
+        self.view.addSubview(animationView!)
+        print("staeted")
+        animationView?.play(completion: { (finished) in
+            print("finished!!")
+        })
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
